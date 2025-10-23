42b8cb3c1272af0713ec84b1b20987ac3ece3970
mitsuo2003 <kevinescubedo@gmail.com>
2025-10-23 15:17:03 +0800

    Update and rename bookingController.js to bookingsController.js

---
+++ src/controllers/bookingsController.js
@@ -0,0 +1,34 @@
+const Booking = require('../models/Booking');
+
+exports.getAllBookings = async (req, res) => {
+  const bookings = await Booking.find().populate('guestId').populate('roomId');
+  res.json(bookings);
+};
+
+exports.createBooking = async (req, res) => {
+  try {
+    const newBooking = new Booking(req.body);
+    await newBooking.save();
+    res.status(201).json(newBooking);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.updateBooking = async (req, res) => {
+  try {
+    const booking = await Booking.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(booking);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.deleteBooking = async (req, res) => {
+  try {
+    await Booking.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Booking deleted' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
